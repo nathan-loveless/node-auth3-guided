9d5ddb94cc4f73892277af7949003ce0b68da43a
Nathan S. Loveless <nloveles@norwich.edu>
2020-02-19 15:58:58 -0500

    Working on guided

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -17,5 +17,29 @@ server.use('/api/users', usersRouter);
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
+
+// server.get('/token', (req, res) => {
+//   // generate a token
+//   // return the token
+//   const token = jwt.sign({
+//     token: 'here it is',
+//     exp: 1000 * 60 *5
+//   }, 'secret')
+//   res.status(400).json(token);
+// })
+
+server.get('/token', (req, res) => {
+  const payload = {
+    subject: 'thisuser',
+    userid: 'skirby',
+    facoriteChili: 'habanero'
+  };
+
+  const secret = 'wethotuwasatoad';
+  const options = { expiresIn: '8h'};
+
+  const token = jwt.sign(payload, secret, options);
+  res.json(token);
+})
 
 module.exports = server;
